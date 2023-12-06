35ae87e72e3c4134b124c347e5331bb11c73cdaa
pkolan <praveen.kolan@t-mobile.com>
2023-12-05 21:47:38 -0500

    Simplepython

---
+++ Simple.py
@@ -0,0 +1,13 @@
+# This program adds two numbers provided by the user
+
+# Prompt the user to enter the first number
+num1 = float(input("Enter first number: "))
+
+# Prompt the user to enter the second number
+num2 = float(input("Enter second number: "))
+
+# Calculate the sum of the two numbers
+sum = num1 + num2
+
+# Display the sum
+print("The sum of", num1, "and", num2, "is", sum)
