23ba22597fad966df1e82e9daefe84b88d406273
preddykolan19 <147754328+preddykolan19@users.noreply.github.com>
2023-12-05 23:14:16 -0500

    Update Simple.py

--- Simple.py
+++ Simple.py
@@ -1,13 +1,16 @@
-# This program adds two numbers provided by the user
+import os
+import sys
 
-# Prompt the user to enter the first number
-num1 = float(input("Enter first number: "))
+# Check if running in a testing environment
+if 'GITHUB_ACTIONS' in os.environ:
+    # In GitHub Actions, use environment variables or default values
+    num1 = float(os.getenv('NUM1', '0'))
+    num2 = float(os.getenv('NUM2', '0'))
+else:
+    # In a normal environment, use input()
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 
-# Prompt the user to enter the second number
-num2 = float(input("Enter second number: "))
-
-# Calculate the sum of the two numbers
 sum = num1 + num2
-
-# Display the sum
 print("The sum of", num1, "and", num2, "is", sum)
+
