019e85497a88a9b8c37472597161f2f19a1d0887
preddykolan19 <147754328+preddykolan19@users.noreply.github.com>
2023-12-15 06:10:17 -0500

    Update obesity_linear.R

--- r-lang-morib-model/obesity_linear.R
+++ r-lang-morib-model/obesity_linear.R
@@ -9,7 +9,8 @@ library(targets)
 tar_plan <- tar_script({
   # Load the dataset
   obesity_data <- read.csv("obesity_data.csv")
-  
+  print("This is a log message1")
+
   # Perform data preprocessing steps if needed
   # For example, handle missing values, transform variables, etc.
   
@@ -17,6 +18,8 @@ tar_plan <- tar_script({
   tar_target(preprocessed_data, {
     # Return the preprocessed data
     return(obesity_data)
+    print("This is a log message2")
+
   })
 })
 
@@ -24,14 +27,18 @@ tar_plan <- tar_script({
 tar_train_model <- tar_script({
   # Load preprocessed data
   preprocessed_data <- tar_read(preprocessed_data)
-  
+  print("This is a log message3")
+
   # Define and train a simple linear regression model
   model <- lm(Mortality ~ Age + EthnicityRace + Geolocation + Income, data = preprocessed_data)
-  
+  print("This is a log message4")
+
   # Save the trained model
   tar_target(trained_model, {
     # Return the trained model
     return(model)
+    print("This is a log message5")
+
   })
 })
 
@@ -41,9 +48,13 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       # Add input controls for age, ethnicity/race, geolocation, and income if needed
+      print("This is a log message6")
+
     ),
     mainPanel(
       plotOutput("prediction_plot")
+      print("This is a log message7")
+
     )
   )
 )
@@ -51,18 +62,24 @@ ui <- fluidPage(
 server <- function(input, output) {
   # Load the trained model
   trained_model <- tar_read(trained_model)
-  
+  print("This is a log message8")
+
   # Define a reactive function for predictions
   predictions <- reactive({
     # Use input values and the trained model for predictions
     # For example: predict(trained_model, newdata = data.frame(Age = input$age, EthnicityRace = input$ethnicity, ...))
   })
-  
+  print("This is a log message9")
+
   # Update the plot based on predictions
   output$prediction_plot <- renderPlot({
     # Plot the predictions or any relevant visualization
-  })
+    
+  })print("This is a log message10")
+
 }
 
 # Run the Shiny app
 shinyApp(ui, server)
+print("This is a log message11")
+
