b2fcb531e235df41ebd34f1ee2719b8f0d3421aa
preddykolan19 <147754328+preddykolan19@users.noreply.github.com>
2023-12-15 11:21:47 -0500

    Update obesity_linear.R

--- r-lang-morib-model/obesity_linear.R
+++ r-lang-morib-model/obesity_linear.R
@@ -1,24 +1,26 @@
 # Install and load required packages
-install.packages(c("tidyverse"))
+install.packages(c("tidyverse", "targets", "shiny","futile.logger"))
+
 
 library(shiny)
 library(tidyverse)
 library(targets)
+library(futile.logger)
 
 # Define the data preprocessing plan using targets
 tar_plan <- tar_script({
   # Load the dataset
   obesity_data <- read.csv("obesity_data.csv")
-  print("This is a log message1")
+  flog.info("Data loaded")
 
   # Perform data preprocessing steps if needed
   # For example, handle missing values, transform variables, etc.
   
   # Save the preprocessed data
   tar_target(preprocessed_data, {
-    # Return the preprocessed data
+    # Return the preprocessed data 
+    flog.info("Preprocessing data")
     return(obesity_data)
-    print("This is a log message2")
 
   })
 })
@@ -27,18 +29,17 @@ tar_plan <- tar_script({
 tar_train_model <- tar_script({
   # Load preprocessed data
   preprocessed_data <- tar_read(preprocessed_data)
-  print("This is a log message3")
+  flog.info("Preprocessing data loaded")
 
   # Define and train a simple linear regression model
   model <- lm(Mortality ~ Age + EthnicityRace + Geolocation + Income, data = preprocessed_data)
-  print("This is a log message4")
+  flog.info("Model trained")
 
   # Save the trained model
   tar_target(trained_model, {
     # Return the trained model
+    flog.info("Save the trained model")
     return(model)
-    print("This is a log message5")
-
   })
 })
 
@@ -48,12 +49,10 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       # Add input controls for age, ethnicity/race, geolocation, and income if needed
-      print("This is a log message6")
 
     ),
     mainPanel(
       plotOutput("prediction_plot")
-      print("This is a log message7")
 
     )
   )
@@ -62,20 +61,20 @@ ui <- fluidPage(
 server <- function(input, output) {
   # Load the trained model
   trained_model <- tar_read(trained_model)
-  print("This is a log message8")
+  flog.info("Trained model loaded")
 
   # Define a reactive function for predictions
   predictions <- reactive({
     # Use input values and the trained model for predictions
     # For example: predict(trained_model, newdata = data.frame(Age = input$age, EthnicityRace = input$ethnicity, ...))
+    browser()  # Start interactive debugging here
   })
-  print("This is a log message9")
 
   # Update the plot based on predictions
   output$prediction_plot <- renderPlot({
     # Plot the predictions or any relevant visualization
-    
-  })print("This is a log message10")
+   
+  })
 
 }
 
